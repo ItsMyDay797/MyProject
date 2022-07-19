f316825f1683aadafc166cbf73f7c7f8e236ac35
ItsMyDay797 <m.rus1298@gmail.com>
2022-07-19 14:19:30 +0300

    I added a new class and MVC

--- MyProject/Controllers/ViewController.swift
+++ MyProject/Controllers/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    class Color {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
